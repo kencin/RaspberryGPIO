57bce3951e4e04b2f950cdbc3664ec3371774c57
kencin <myzincx@gmail.com>
2018-12-29 13:42:32 +0800

    Initial commit

---
+++ AliyunIOT/config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-  
+# RaspberryGPIO - config.py.py
+# 2018/12/14 17:51
+# Author:Kencin <myzincx@gmail.com>
+
+IOT = {
+    "PRODUCE_KEY": "a1AS1zAhWfq",
+    "DEVICE_NAME": "wGSNoyc0ojrdkzto27VS",
+    "DEVICE_SECRET": "UKeL0p7hI7DVtaI22atUyJ1g90kViyia"
+}
